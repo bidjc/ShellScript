e6181d14de0260f3bc353603a8213525932c1e7c
bidjc <563200085@qq.com>
2016-04-07 18:49:28 +0800

    Revert "脚本"
    This reverts commit 46291d815c7b2e5d43dccf44e3a43d409e08e8d3.

--- kill.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-k1=`ps | grep "/root/Box/BoxPlayer/BoxIOServices"|grep -v grep|awk '{print $1}'`
-echo $k1
-kill -9 $k1
-k2=`ps | grep "/root/Box/BoxPlayer/BoxPlayer"|grep -v grep|awk '{print $1}'`
-echo $k2
-kill -9 $k2
